7377f815d6bdb3a3a2e7795fdaa979d2baede8ed
leh11 <ui@laccanu.ufal.br>
2022-04-01 02:44:17 -0300

    description: update done

--- src/main/java/com/bioksam/smarbr/CarouselView.java
+++ src/main/java/com/bioksam/smarbr/CarouselView.java
@@ -1,11 +1,13 @@
 package com.bioksam.smarbr;
 
 import jakarta.faces.view.ViewScoped;
+import org.primefaces.model.ResponsiveOption;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Named
@@ -14,18 +16,19 @@ public class CarouselView implements Serializable {
 
     private List<Product> products;
 
-    //private List<ResponsiveOption> responsiveOptions;
+    private List<ResponsiveOption> responsiveOptions;
 
     @Inject
     private ProductService service;
 
     @PostConstruct
     public void init() {
-        products = service.getProducts(9);
-        /*responsiveOptions = new ArrayList<>();
-        responsiveOptions.add(new ResponsiveOption("1024px", 3, 3));
-        responsiveOptions.add(new ResponsiveOption("768px", 2, 2));
-        responsiveOptions.add(new ResponsiveOption("560px", 1, 1));*/
+        products = service.getProducts(12);
+        responsiveOptions = new ArrayList<>();
+        responsiveOptions.add(new ResponsiveOption("1024px", 4, 4));
+        responsiveOptions.add(new ResponsiveOption("880px", 3, 3));
+        responsiveOptions.add(new ResponsiveOption("622px", 2, 2));
+        responsiveOptions.add(new ResponsiveOption("560px", 1, 1));
     }
 
     public List<Product> getProducts() {
@@ -35,12 +38,11 @@ public class CarouselView implements Serializable {
     public void setService(ProductService service) {
         this.service = service;
     }
-/*
     public List<ResponsiveOption> getResponsiveOptions() {
         return responsiveOptions;
     }
 
     public void setResponsiveOptions(List<ResponsiveOption> responsiveOptions) {
         this.responsiveOptions = responsiveOptions;
-    }*/
+    }
 }
